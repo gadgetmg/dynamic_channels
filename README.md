e41fef333ac398d8656f9e65eb1c7d8f5a1519fa
Matt Graham <gadgetmg@gmail.com>
2019-03-31 21:12:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Dynamic Channels Discord bot
+
+This bot manages dynamic groups of voice channels in a Discord server.
+
+On channels named in the format of `Name Number` the bot will automatically
+create and delete channels as members join and leave them.
+
+## Requirements
+
+The script requires the latest version of the `discord.py` module. This module
+is still in development and must be installed from its git repo:
+
+```
+python3 -m pip install -U git+https://github.com/Rapptz/discord.py
+```
+
+## Permissions
+
+The bot needs only the **Manage Channels** permissionr. On private channels, it
+must also have the **Connect** permission explicitly granted.
+
+## Usage
+
+Run the bot's script with the `python3` command specifing the bot's API key as
+an environment variable.
+
+```bash
+API_KEY=SECRET python3 bot.py
+```
+
+## Installing as a service
+
+The bot should normally run as a service. As an example, this can be configured
+as a `systemd` service.
+
+Create the service at ```/lib/systemd/system/dynamic_channels.service```.
+
+```
+[Unit]
+Description=Dynamic Channels Discord bot
+
+[Service]
+Type=simple
+Environment="API_KEY=SECRET"
+ExecStart=/usr/bin/python3 /opt/dynamic_channels/bot.py
+
+[Install]
+WantedBy=multi-user.target
+```
+
+Then, start the service and configure it to run on startup.
+
+```
+sudo systemctl enable dynamic_channels
+sudo systemctl start dynamic_channels
+```
+
+## Contributing
+
+1. Create a named feature branch (i.e. `add-feature`)
+2. Write your change
+3. Write tests for your change (if applicable)
+4. Run the tests, ensuring they all pass
+5. Submit a Merge Request
+
+## License and Authors
+
+Author: Matt Graham (<gadgetmg@gmail.com>)
+
+Copyright (c) 2019 Matt Graham, Licensed under the Apache License 2.0
---
+++ bot.py
@@ -0,0 +1,82 @@
+import discord
+import os
+import re
+
+class Bot(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_voice_state_update(self, member, before, after):
+        # Get the channel's basename
+        def get_basename(channel):
+            match = re.search("^(.*) [0-9]*$", channel.name)
+            if match is not None:
+                return match.group(1)
+
+        # Get the channel's iteration
+        def get_iteration(channel):
+            match = re.search("^.* ([0-9]*)$", channel.name)
+            if match is not None:
+                return int(match.group(1))
+
+        # If the channels match, the member only muted or unmuted
+        if before.channel == after.channel:
+            return
+
+        # Channel just joined
+        if after.channel and re.match("^.* [0-9]*$", after.channel.name):
+            # Add a channel if this was the first member
+            if len(after.channel.members) == 1:
+                channel = await member.guild.create_voice_channel(
+                    # Create with all the attributes from the current channel
+                    name=get_basename(after.channel) + " " + str(get_iteration(after.channel) + 1),
+                    overwrites=dict(after.channel.overwrites),
+                    category=after.channel.category,
+                    position=after.channel.position,
+                    bitrate=after.channel.bitrate,
+                    user_limit=after.channel.user_limit
+                )
+            # In some cases, the API won't respect the position of the new channel
+            if channel.position != after.channel.position:
+                await channel.edit(
+                    # When editing, position has to be 1 greater
+                    position=after.channel.position + 1
+                )
+
+        # Channel just left
+        if before.channel and re.match("^.* [0-9]*$", before.channel.name):
+            def filter_channel_group(channel):
+                # Channel must be in the same category
+                if channel.category_id != before.channel.category_id:
+                    return False
+                # Basenames have to match
+                elif get_basename(channel) != get_basename(before.channel):
+                    return False
+                else:
+                    return True
+
+            # Get a list of all the channels in the group
+            channel_group = list(filter(filter_channel_group, member.guild.channels))
+
+            empty = 0
+            for channel in channel_group:
+                # Count empty channels
+                if len(channel.members) == 0:
+                    empty += 1
+
+            iteration = 1
+            for channel in channel_group:
+                # Delete all but one of the empty channels
+                if empty > 1 and len(channel.members) == 0:
+                    await channel.delete()
+                    empty -= 1
+                else:
+                    # Renumber the rest of the channels to be sequential
+                    if get_iteration(channel) != iteration:
+                        await channel.edit(
+                            name=get_basename(channel) + " " + str(iteration)
+                        )
+                    iteration += 1
+
+bot = Bot()
+bot.run(os.environ.get('API_KEY'))
